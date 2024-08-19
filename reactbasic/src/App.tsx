8b58e2d853d14c48c5b9af62b88fe88cb0705030
Jeongsunga <jsa3341@gmail.com>
2024-08-19 12:03:33 +0900

    2024-08-19 11:50

--- reactbasic/src/App.tsx
+++ reactbasic/src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {ClassComponent} from './component/Component'
+import Component, {ClassComponent, FunctionComponent} from './component/Component' 
+// export default X (여러 요소 export 가능) -> import {하나의 요소}(이름 변경 시 as 사용) from ...  
+// export default O (반드시 하나의 요소만 export) -> import 하나의 요소(이름 변경 가능) from ...
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Component/>
       <ClassComponent/>
+      <FunctionComponent/>
     </div>
   );
 }
