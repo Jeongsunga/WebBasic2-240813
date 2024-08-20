eb6c087d30fe6c119d6c886dd45044f84743f525
Jeongsunga <jsa3341@gmail.com>
2024-08-20 17:04:36 +0900

    24-08-20 16:50

--- springbasic/src/main/java/com/jsa/springbasic/controller/BasicController.java
+++ springbasic/src/main/java/com/jsa/springbasic/controller/BasicController.java
@@ -2,6 +2,11 @@ package com.jsa.springbasic.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 // Controller 레이어
@@ -25,4 +30,38 @@ public class BasicController {
     }
 
     // HTTP GET method : 클라이언트가 서버로부터 데이터를 받기 위한 메서드 (Request Body X)
+    @GetMapping("")
+    public String getMethod() {   //  GET http://localhost:4000/basic
+        return "GET Method";
+    }
+    
+    // HTTP POST method : 클라이언트가 서버에 리소스를 작성하기 위한 메서드 (Request Body O)
+    @PostMapping("")
+    public String postMethod() {   //  POST http://localhost:4000/basic
+        return "POST Method";
+    }
+
+    // HTTP PUT method : 클라이언트가 서바에 리소스를 전체 수정하기 위한 메서드 (Request Body O)
+    @PutMapping("")
+    public String putMethod() {   //  PUT http://localhost:4000/basic
+        return "PUT Method";
+    }
+
+    // HTTP PATCH method : 클라이언트가 서버에 리소스를 일부 수정하기 위한 메서드 (Request Body O)
+    @PatchMapping("")
+    public String patchMethod() {   //  PATCH http://localhost:4000/basic
+        return "PATCH Method";
+    }
+
+    // HTTP DELETE method : 클라이언트가 서버에 리소스를 삭제하기 위한 메서드 (Request Body O)
+    @DeleteMapping("")
+    public String deleteMethod() {   //  DELETE http://localhost:4000/basic
+        return "DELETE Method";
+    }
+
+    // Method + URL Pattern이 중복되면 서버 실행 중 에러 발생
+    // @GetMapping("/first")
+    // public String getFirst(){
+    //     return "get first";
+    // }
 }
