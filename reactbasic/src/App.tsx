c026079f28cd8600243da183068adb38e24652a2
Jeongsunga <jsa3341@gmail.com>
2024-08-20 14:18:57 +0900

    24-08-20 12:50

--- reactbasic/src/App.tsx
+++ reactbasic/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import CurlyBraces from './component/CurlyBraces';
 import Component, {ClassComponent, FunctionComponent} from './component/Component' 
+import Properties from './component_manage/Properties';
+import ConditionalRender from './component_manage/ConditionalRender';
 // export default X (여러 요소 export 가능) -> import {하나의 요소}(이름 변경 시 as 사용) from ...  
 // export default O (반드시 하나의 요소만 export) -> import 하나의 요소(이름 변경 가능) from ...
 import './App.css';
-import Properties from './component_manage/Properties';
-import ConditionalRender from './component_manage/ConditionalRender';
+import Example2 from './example/Example2';
 
 function App() {
   return (
@@ -14,8 +15,9 @@ function App() {
       <ClassComponent/>
       <FunctionComponent/>
       <CurlyBraces/>
-      <Properties/> */}
-      <ConditionalRender/>
+      <Properties/>
+      <ConditionalRender/> */}
+      <Example2/>
     </div>
   );
 }
