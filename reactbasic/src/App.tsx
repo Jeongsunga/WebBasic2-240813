40061421eadc2dadae1cbe34c91ba0e2decb1efb
Jeongsunga <jsa3341@gmail.com>
2024-08-29 11:03:35 +0900

    24-08-23 10:50

--- reactbasic/src/App.tsx
+++ reactbasic/src/App.tsx
@@ -14,10 +14,58 @@ import ForwardingStateComponent from './interaction/ForwardingStateComponent';
 import HookComponent from './hook/HookComponent';
 import HookComponent2 from './hook/HookComponent2';
 import CustomHook from './hook/CustomHook';
+import { Outlet, Route, Routes } from 'react-router';
+import QueryString from './router/QueryString';
+import { PathVariable } from './router/PathVariable';
+
+// react-router 패키지
+// - react의 SPA(Single Page Application)에서 라우팅을 구현하기 위한 라이브러리
+// - 웹 페이지에서 다양한 경로에 따른 뷰를 관리할 수 있도록 도와줌
+// - 웹 애플리케이션 내에서 URL 이동이 있을 때 브라우저를 새로고침 하지 않음(서버로 새로운 요청을 보내지 않음)
+// - 웹 애플리케이션 내에서 네비게이션 역할을 수행
+
+// - npm install react-router react-router-dom
+
+// - root 경로에 있는 index.tsx의 root.render() 안에 <App /> 컴포넌트를 <BrowserRouter> 컴포넌트로 묶어야 함
+// - <BrowserRouter> : URL을 사용하여 브라우저 주소 표시줄에 현재 위치를 저장하고 탐색할 수 있도록 하는 컴포넌트
+
+// - 특정 URL 패턴에 대해서 컴포넌트를 각각 다르게 랜더링하고자 한다면 App.tsx에 <Routes>와 <Route> 컴포넌트를 사용하여 경로에 따라 컴포넌트를 랜더링할 수 있음
+
+// <Routes> : <Route> 컴포넌트로 URL에 따른 컴포넌트 랜더링을 할 수 있도록 하는 컴포넌트
+// <Route> : URL 패턴에 따라 랜더링하고자 하는 컴포넌트를 지정하는 컴포넌트
+// - path 속성 : URL 패턴을 지정
+// - element 속성 : 랜더링할 컴포넌트 지정
+// - index 속성 : 현재 경로의 기본 라우터로 지정
+
+function Layout() {
+  // <Outlet> : 부모 <Route>에 해당 컴포넌트가 element로 등록되었을 때 
+  //            자식 <Route>의 element가 해당 위치에 랜더링 되도록 하는 컴포넌트
+  return (
+    <div>
+      <div style={{height:'100px', backgroundColor: 'red'}}></div>
+      <Outlet/>
+      <div style={{height:'100px', backgroundColor: 'blue'}}></div>
+    </div>
+  )
+}
+
 
 function App() {
   return (
-    <div>
+    <Routes>
+      <Route index element={<h1>기본 페이지</h1>}/>
+      <Route path='/component' element={<Layout/>}>
+        <Route index element={<Component/>}/>
+        <Route path='function-component' element={<FunctionComponent/>}/>
+        <Route path='curly-braces' element={<CurlyBraces/>}/>   {/* 주의) / 붙이지 않기! */}
+      </Route>
+      <Route path='/router'>
+        {/* localhost:3000/router/query-string?name=홍길동&age=23 */}
+        <Route path='query-string' element={<QueryString/>}/>
+        {/* localhost:3000/router/path-variable/홍길동 */}
+        <Route path='path-variable/:name' element={<PathVariable/>}/>   {/* name 활용시 : 사용 -> 컴포넌트 이름과 같아야 함! */}
+      </Route>
+      <Route path='*' element={<h1>404!</h1>}/>    {/* 지정하지 않은 URL에 대한 대처 */}
       {/* <Component/> */}
       {/* <ClassComponent/> */}
       {/* <FunctionComponent/> */}
@@ -31,8 +79,8 @@ function App() {
       {/* <ForwardingStateComponent/> */}
       {/* <HookComponent/> */}
       {/* <HookComponent2/> */}
-      <CustomHook/>
-    </div>
+      {/* <CustomHook/> */}
+    </Routes>
   );
 }
 
