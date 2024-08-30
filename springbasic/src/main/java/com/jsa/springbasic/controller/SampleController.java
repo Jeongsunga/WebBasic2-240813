d6acaad09c4a3a3c72010782186be04eb7fd1b02
Jeongsunga <jsa3341@gmail.com>
2024-08-30 09:27:44 +0900

    24-08-29 17:50

--- springbasic/src/main/java/com/jsa/springbasic/controller/SampleController.java
+++ springbasic/src/main/java/com/jsa/springbasic/controller/SampleController.java
@@ -44,4 +44,12 @@ public class SampleController {
         ResponseEntity<String> response = sampleService.queryString();
         return response;
     }
+
+    @GetMapping("/jwt/{name}")
+    public String getJwt(
+        @PathVariable("name") String name
+    ){
+        String response = sampleService.getJwt(name);
+        return response;
+    }
 }
